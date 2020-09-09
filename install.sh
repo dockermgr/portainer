334ca3cef8709958b13faedea492d1f6bf4901bd
casjay <casjay@yahoo.com>
2020-09-09 15:51:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir -p /srv/docker/portainer && chmod -Rf 777 /srv/docker/portainer
+APPNAME="portainer-ce"
+DATADIR="/srv/docker/$APPNAME"
 
+mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+docker pull portainer/portainer-ce && docker restart $APPNAME
+else
 docker run -d -p 127.0.0.1:9010:9000 \
 --restart always \
---name portainer \
+--name "$APPNAME" \
+-v $DATADIR:/data \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v /srv/docker/portainer:/data \
 portainer/portainer-ce
+fi
