aab6a49ba25e342748dc543db73c6aae0d3cfe01
Jason <jason@casjay.net>
2021-01-29 23:28:02 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 APPNAME="portainer-ce"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker stop "$APPNAME"
-docker rm -f "$APPNAME"
-docker pull portainer/portainer-ce
+  docker stop "$APPNAME"
+  docker rm -f "$APPNAME"
+  docker pull portainer/portainer-ce
+else
+  docker run -d \
+    -p 8000:8000 \
+    -p 9010:9000 \
+    --restart always \
+    --name "$APPNAME" \
+    -v "$DATADIR":/data \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    portainer/portainer-ce
 fi
-docker run -d \
--p 8000:8000 \
--p 9010:9000 \
---restart always \
---name "$APPNAME" \
--v $DATADIR:/data \
--v /var/run/docker.sock:/var/run/docker.sock \
-portainer/portainer-ce
