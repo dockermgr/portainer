52e52acf9fc87a181b7999b469986b5ae94a1a81
Jason <jason@casjay.net>
2021-07-10 15:54:49 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 APPNAME="portainer-ce"
+DOCKER_HUB_URL="portainer/portainer-ce"
 
-mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR"
+sudo chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  docker stop "$APPNAME"
-  docker rm -f "$APPNAME"
-  docker pull portainer/portainer-ce
+  sudo docker stop "$APPNAME"
+  sudo docker rm -f "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
-  docker run -d \
+  sudo docker run -d \
     -p 8000:8000 \
     -p 9010:9000 \
     --restart always \
     --name "$APPNAME" \
     -v "$DATADIR":/data \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    portainer/portainer-ce
+    "$DOCKER_HUB_URL"
 fi
